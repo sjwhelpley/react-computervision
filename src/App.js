1bf36f4213fd50ee23b5f5b8fac776271f3fea46
Samantha Whelpley <sjw86@pitt.edu>
2022-06-12 15:29:34 -0400

    Fix URL

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Webcam from "react-webcam";
 import axios from 'axios';
 
 const http = axios.create({
-  baseURL: "https://poemotion-api.herokuapp.com//api",
+  baseURL: "https://poemotion-api.herokuapp.com/api",
   // baseURL: "http://localhost:8080/api",
   headers: {
     "Content-type": "application/json"
